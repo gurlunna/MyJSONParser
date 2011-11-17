6baf40403c8ee7c68eb724b896f05b525bb9c58d
Guri Lunnan <gurilunnan@gmail.com>
2011-11-17 10:08:05 +0100

    Fikset filnavn, tegnsett, gitignore

--- src/com.popsenteret.jsonparser/MyJsonParser.java
+++ src/com.popsenteret.jsonparser/MyJsonParser.java
@@ -31,7 +31,7 @@ public class MyJsonParser {
 
     // Reads input from file to String
     public static String readJSONFromFile(String infileName) throws IOException {
-        BufferedReader fr = new BufferedReader(new FileReader(infileName));
+        BufferedReader fr = new BufferedReader(new InputStreamReader(new FileInputStream(infileName), "UTF-8"));
         StringBuffer stringBuffer = new StringBuffer();
         String dataFromFile;
 
@@ -42,25 +42,44 @@ public class MyJsonParser {
         return stringBuffer.toString();
     }
 
+    public static String formatFileName(String s) {
+        String formatted = s.toLowerCase();
+        formatted = formatted.replace(" ", "_");
+        formatted = formatted.replace("&", "and");
+        formatted = formatted.replace(".", "");
+        formatted = formatted.replace("'", "");
+        formatted = formatted.replace("(", "_");
+        formatted = formatted.replace(")", "_");
+        formatted = formatted.replace("/", "_");
+        formatted = formatted.replace("\\", "_");
+        formatted = formatted.replace("æ", "_");
+        formatted = formatted.replace("ø", "_");
+        formatted = formatted.replace("å", "_");
+        formatted = formatted.replace("ð", "_");
+        formatted = formatted.replace("`", "");
+        formatted = formatted.replace("´", "");
+        formatted = formatted.replace("ö", "o");
+        formatted = formatted.replace("ü", "u");
+        formatted = formatted.replace("$", "");
+        formatted = formatted.replace("@", "");
+        return formatted;
+    }
+
     // Writes the tracks to the outputfile
     public static void writeBatFile(String outfileName, List<Track> tracks) throws IOException {
-        PrintWriter printWriter = new PrintWriter(new File(outfileName));
+        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outfileName), "UTF8"));
         String script1 = "convert -background black -gravity Center -density 100 -fill white  -font \"C:\\Windows\\Fonts\\alternategothic2_bt.ttf\" -size 600x60 label:\"";
         String script2 = "\" label.gif";
         String script3 = "composite -gravity South label.gif";
-        int i = 1;
         printWriter.println("chcp 65001 &");
-        for (Track t : tracks) {
-            String artist = t.getArtistName().replace(" ", "_");
-            artist = artist.replace("&", "and");
-            artist = artist.replace(".", "");
-            String title = t.getTitle().replace(" ", "_");
-            title = title.replace("&", "and");
-            title = title.replace("/", "_");
-            title = title.replace("\\", "_");
-            printWriter.println(script1 + t.getArtistName().toUpperCase() + " - " + t.getTitle() + script2);
-            printWriter.println(script3 + " " + ("FraNRK/" + artist + "_" + t.getImageName(t.getImageXLURL())) + " " + "TilPopit/" + artist + "-" + title + "_" + t.getImageNameWithText(t.getImageXLURL()));
-            i++;
+        for (Track track : tracks) {
+            String artist = formatFileName(track.getArtistName());
+            String title = formatFileName(track.getTitle());
+            System.out.println(artist + " " + title);
+            String[] temp = track.getImageName(track.getURL()).split("[.]");
+            String imageName = temp[0] + ".jpg";
+            printWriter.println(script1 + track.getArtistName().toUpperCase() + " - " + track.getTitle() + script2);
+            printWriter.println(script3 + " " + "FraNRK" + File.separator + artist + "_" + track.getImageName(track.getImageXLURL()) + " " + "TilPopit" + File.separator + artist + "-" + title + "_" + imageName);
         }
         printWriter.println("@DEL /S /Q /F \"FraNRK\\*.*\"");
         printWriter.println("& chcp 850");
